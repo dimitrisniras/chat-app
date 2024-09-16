ece622df92904acb2aba8fe3b0c24f9aa10c8bfb
Dimitris Niras <niras.94@gmail.com>
2024-09-16 17:02:18 +0300

    Add mongodb as the persistence layer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod init; 
+mod db;
+mod errors;
+mod logging;
+mod models;
+
+use log::error;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Initialize the application
+    if let Err(err) = init::init().await {
+        error!("Initialization failed: {}", err);
+        return Err(err); 
+    }
+
+    Ok(())
 }
