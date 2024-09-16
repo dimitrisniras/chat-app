539d33bb63121cfebad19f134a816470606ca01c
Dimitris Niras <niras.94@gmail.com>
2024-09-16 17:34:55 +0300

    Add Github Actions CI/CD and Makefile (#5)

--- src/init.rs
+++ src/init.rs
@@ -1,17 +1,17 @@
 use crate::db;
 use crate::logging;
 
-use std::env;
 use anyhow::Context;
-use dotenv::dotenv;
-use log::{info, error};
-use mongodb::Client;
 use anyhow::Result;
+use dotenv::dotenv;
+use log::{error, info};
+use mongodb::Client;
+use std::env;
 
 pub async fn init() -> Result<(Client, String, u16)> {
     if let Err(err) = dotenv() {
         error!("Failed to load environment variables: {}", err);
-        return Err(err.into()); 
+        return Err(err.into());
     }
 
     logging::init();
@@ -19,9 +19,8 @@ pub async fn init() -> Result<(Client, String, u16)> {
 
     let db_client = db::init().await?;
 
-    let url = env::var("SERVER_URL")
-        .context("SERVER_URL environment variable not set")?;
-    
+    let url = env::var("SERVER_URL").context("SERVER_URL environment variable not set")?;
+
     let port = env::var("SERVER_PORT")
         .context("SERVER_PORT environment variable not set")?
         .parse::<u16>()
